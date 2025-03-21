fbc1c159cc5923e5069b174373c996efc94fffef
Zetuxx <damian.gajda-2000@wp.pl>
2025-03-22 00:05:43 +0100

    Implement node connection movement and page styling

--- berger/Pages/GraphEditor.xaml.cs
+++ berger/Pages/GraphEditor.xaml.cs
@@ -64,18 +64,22 @@ namespace berger.Pages
                 createdLine.Y2 = position.Y;
             }
         }
+
         private void Elipse_Click_Right(object sender, MouseButtonEventArgs e)
         {
             if (sender is Ellipse clickedEllipse)
             {
                 if (createdLine == null)
                 {
-                    Line line = new Line();
-                    line.Stroke = Brushes.Black;
-                    line.X1 = Canvas.GetLeft(clickedEllipse) + clickedEllipse.Width / 2;
-                    line.Y1 = Canvas.GetTop(clickedEllipse) + clickedEllipse.Height / 2;
-                    line.X2 = Canvas.GetLeft(clickedEllipse) + clickedEllipse.Width / 2;
-                    line.Y2 = Canvas.GetTop(clickedEllipse) + clickedEllipse.Height / 2;
+                    Line line = new()
+                    {
+                        Stroke = Brushes.Black,
+                        X1 = Canvas.GetLeft(clickedEllipse) + clickedEllipse.Width / 2,
+                        Y1 = Canvas.GetTop(clickedEllipse) + clickedEllipse.Height / 2,
+                        X2 = Canvas.GetLeft(clickedEllipse) + clickedEllipse.Width / 2,
+                        Y2 = Canvas.GetTop(clickedEllipse) + clickedEllipse.Height / 2
+                    };
+
                     GraphCanvas.Children.Add(line);
                     createdLine = line;
                     firstRightClickedElipse = clickedEllipse;
@@ -89,12 +93,17 @@ namespace berger.Pages
                         firstRightClickedElipse = null;
                         return;
                     }
+
                     createdLine.X2 = Canvas.GetLeft(clickedEllipse) + clickedEllipse.Width / 2;
                     createdLine.Y2 = Canvas.GetTop(clickedEllipse) + clickedEllipse.Height / 2;
+
+                    createdLine.Tag = Tuple.Create(firstRightClickedElipse, clickedEllipse);
+                    AddLineToConnections(firstRightClickedElipse, createdLine);
+                    AddLineToConnections(clickedEllipse, createdLine);
+
                     createdLine = null;
                     actionsAfterCreateLine();
                 }
-
             }
         }
         private void createNodeOnWorkspace(Point position, int width, int height, SolidColorBrush colorBrush)
@@ -117,6 +126,9 @@ namespace berger.Pages
 
         }
 
+
+        private Dictionary<Ellipse, List<Line>> nodeConnections = [];
+
         private void Canvas_MouseMove(object sender, MouseEventArgs e)
         {
             if (selectedNode != null)
@@ -124,8 +136,42 @@ namespace berger.Pages
                 Point position = e.GetPosition(GraphCanvas);
                 Canvas.SetLeft(selectedNode, position.X - selectedNode.Width / 2);
                 Canvas.SetTop(selectedNode, position.Y - selectedNode.Height / 2);
+
+                if (nodeConnections.TryGetValue(selectedNode, out List<Line> lines))
+                {
+                    foreach (var line in lines)
+                    {
+                        if (line.Tag is Tuple<Ellipse, Ellipse> endpoints)
+                        {
+                            if (endpoints.Item1 == selectedNode)
+                            {
+                                line.X1 = position.X;
+                                line.Y1 = position.Y;
+                            }
+                            else if (endpoints.Item2 == selectedNode)
+                            {
+                                line.X2 = position.X;
+                                line.Y2 = position.Y;
+                            }
+                        }
+                    }
+                }
             }
         }
+
+ 
+
+        private void AddLineToConnections(Ellipse node, Line line)
+        {
+            if (!nodeConnections.TryGetValue(node, out List<Line>? value))
+            {
+                value = [];
+                nodeConnections[node] = value;
+            }
+
+            value.Add(line);
+        }
+
         private void Canvas_MouseUp(object sender, MouseButtonEventArgs e)
         {
             if (e.MiddleButton == MouseButtonState.Released)
